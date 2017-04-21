83420220a63afcd8922ed6fe464182ea0ca4fc61
Alonso Cauich <alonsocauichv1@gmail.com>
2017-04-21 10:04:32 -0600

    css
    css

--- resources/views/layouts/partials/bannertwo.blade.php
+++ resources/views/layouts/partials/bannertwo.blade.php
@@ -1,21 +1,21 @@
 <section id="main-slider">
     <div class="owl-carousel">
-        <div class="item" style="background-image: url(slider/bg1.jpg);">
+        <div class="item" style="">
             <div class="slider-inner">
                 <div class="container">
                     <div class="row">
                         <div class="col-md-12">
                             <div class="carousel-content text-center">
                                 <h2><span>¿Quiénes Somos?</span></h2>
-                                <p>Somos una empresa especializada en brindar soluciones de conectividad inalámbrica WiFi en ambientes de alta densidad de usuarios: universidades, hospitales, hoteles, aeropuertos entre otros en donde SitWifi se encarga de la operación, mantenimiento y administración de la infraestructura garantizando la disponibilidad de la misma por contrato.</p>
-                                <a class="btn btn-primary btn-lg" href="#">Read More</a>
+                                <p style="padding-top: 20px; text-align: justify;  line-height: 2;">Somos una empresa especializada en brindar soluciones de conectividad inalámbrica WiFi en ambientes de alta densidad de usuarios: universidades, hospitales, hoteles, aeropuertos entre otros en donde SitWifi se encarga de la operación, mantenimiento y administración de la infraestructura garantizando la disponibilidad de la misma por contrato.</p>
+                                <!--<a class="btn btn-primary btn-lg" href="#">Read More</a>-->
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div><!--/.item-->
-        <div class="item" style="background-image: url(slider/bg2.jpg);">
+        <div class="item" style="">
             <div class="slider-inner">
                 <div class="container">
                     <div class="row">
@@ -31,7 +31,7 @@
                 </div>
             </div>
         </div><!--/.item-->
-        <div class="item" style="background-image: url(slider/bg3.jpg);">
+        <div class="item" style="">
             <div class="slider-inner">
                 <div class="container">
                     <div class="row">
