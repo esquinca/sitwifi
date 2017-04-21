e41d59fa9e21761642f0f5db1775d090c5344610
Alonso Cauich <alonsocauichv1@gmail.com>
2017-04-21 11:15:53 -0600

    image responsive
    image responsive slider

--- resources/views/layouts/partials/bannertwo.blade.php
+++ resources/views/layouts/partials/bannertwo.blade.php
@@ -38,14 +38,31 @@
                             <div class="carousel-content text-center">
                                 <h2>{{ trans('text.title_six')}} <span></span></h2>
                                 <p style="padding-top: 10px; text-align: justify;  line-height: 2;">{{ trans('text.content_six')}} </p>
-                                <div>
-                                  <img src="../img/clientes/client19.png" alt="Palace Recorts">
-                                  <img src="../img/clientes/client21.png" alt="UVM">
-                                  <img src="../img/clientes/client13.png" alt="UNITEC">
-                                  <img src="../img/clientes/cliente_30.png" alt="XCARET">
-                                  <img src="../img/clientes/client05.png" alt="Karisma">
+                                <div class="container">
+                                  <div class="row">
+                                    <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+                                      <img class="img-responsive" src="../img/clientes/client19.png" alt="Palace Recorts">
+                                    </div>
+                                    <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+                                      <img class="img-responsive" src="../img/clientes/client21.png" alt="UVM">
+                                    </div>
+                                    <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+                                      <img class="img-responsive" src="../img/clientes/client13.png" alt="UNITEC">
+                                    </div>
+                                    <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+                                      <img class="img-responsive" src="../img/clientes/cliente_30.png" alt="XCARET">
+                                    </div>
+                                    <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+                                      <img class="img-responsive" src="../img/clientes/client05.png" alt="Karisma">
+                                    </div>
+                                    <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+                                      <img class="img-responsive" src="../img/clientes/client20.png" alt="H10">
+                                    </div>
+                                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                                      <a id="" class="btn btn-success  btn-lg"><i class="fa fa-bookmark-o"></i> {{ trans('text.title_b')}}</a>
+                                    </div>
+                                  </div>
                                 </div>
-                                <a id="" class="btn btn-success  btn-lg"><i class="fa fa-bookmark-o"></i> {{ trans('text.title_b')}}</a>
                             </div>
                         </div>
                     </div>
