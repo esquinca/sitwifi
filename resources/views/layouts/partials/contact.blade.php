4034e6933bd6d3c7f2dc23f48ed6bc261029f1cd
Alonso Cauich <alonsocauichv1@gmail.com>
2017-04-22 11:00:57 -0600

    modif
    Modificacion de contacto nueva

--- resources/views/layouts/partials/contact.blade.php
+++ resources/views/layouts/partials/contact.blade.php
@@ -23,22 +23,42 @@
             <form action="" method="post" role="form" class="contactForm">
                 <div class="col-md-6 padding-right-zero">
                     <div class="form-group">
-                        <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                        <input type="text" name="name" class="form-control" id="name" placeholder="Ingrese su nombre" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
                         <div class="validation"></div>
                     </div>
                 </div>
-                <div class="col-md-6">
+                <div class="col-md-6 padding-right-zero">
                     <div class="form-group">
-                        <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
+                        <input type="text" class="form-control" name="empresa" id="empresa" placeholder="Nombre de la Empresa" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
                         <div class="validation"></div>
                     </div>
                 </div>
-                <div class="col-md-12">
+                <div class="col-md-6 padding-right-zero">
+                    <div class="form-group">
+                        <input type="email" name="email" class="form-control" id="email" placeholder="Ingrese el correo electronico" data-rule="email" data-msg="Please enter a valid email" />
+                        <div class="validation"></div>
+                    </div>
+                </div>
+                <div class="col-md-6 padding-right-zero">
                     <div class="form-group">
                         <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
                         <div class="validation"></div>
                     </div>
                 </div>
+
+                <div class="col-md-12">
+                  <div class="form-group">
+                    <select  name="producto" id="producto" class="form-control" data-rule="required:true" data-msg="Please choose a element">
+                      <option value="">Selecciona un producto</option>
+                      <option value="1">Wifi Administrado</option>
+                      <option value="1">Venta  de infraestructura WiFi</option>
+                      <option value="1">Publicidad con WifiMedia</option>
+                      <option value="1">Otros</option>
+                    </select>
+                    <div class="validation"></div>
+                  </div>
+                </div>
+
                 <div class="col-md-12">
                   <div class="form-group">
                         <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
