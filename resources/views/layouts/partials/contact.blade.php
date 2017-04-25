1b899c725ba50c95dc0207084e9dfcebf8261e3d
Unknown <esquinca_jose@hotmail.com>
2017-04-25 18:24:59 -0500

    cambio de texto, area contacto

--- resources/views/layouts/partials/contact.blade.php
+++ resources/views/layouts/partials/contact.blade.php
@@ -20,36 +20,36 @@
                 {{ csrf_field() }}
                 <div class="col-md-6 padding-right-zero">
                     <div class="form-group">
-                        <input type="text" name="name" class="form-control" id="name" placeholder="Ingrese su nombre" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                        <input type="text" name="name" class="form-control" id="name" placeholder="Ingrese su nombre." data-rule="minlen:4" data-msg="Por favor ingrese al menos 4 caracteres" />
                         <div class="validation"></div>
                     </div>
                 </div>
                 <div class="col-md-6 padding-right-zero">
                     <div class="form-group">
-                        <input type="text" class="form-control" name="empresa" id="empresa" placeholder="Nombre de la Empresa" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                        <input type="text" class="form-control" name="empresa" id="empresa" placeholder="Nombre de la Empresa." data-rule="minlen:4" data-msg="Por favor ingrese al menos 4 caracteres" />
                         <div class="validation"></div>
                     </div>
                 </div>
                 <div class="col-md-6 padding-right-zero">
                     <div class="form-group">
-                        <input type="email" name="email" class="form-control" id="email" placeholder="Ingrese el correo electronico" data-rule="email" data-msg="Please enter a valid email" />
+                        <input type="email" name="email" class="form-control" id="email" placeholder="Ingrese su correo electrónico." data-rule="email" data-msg="Por favor ingrese un correo valido." />
                         <div class="validation"></div>
                     </div>
                 </div>
                 <div class="col-md-6 padding-right-zero">
                     <div class="form-group">
-                        <input type="number" class="form-control" name="subject" id="telefono" placeholder="telefono" data-rule="minlen:10" data-msg="Please enter at least 10 chars" />
+                        <input type="number" class="form-control" name="subject" id="telefono" placeholder="Teléfono." data-rule="minlen:10" data-msg="Por favor ingrese al menos 10 caracteres." />
                         <div class="validation"></div>
                     </div>
                 </div>
 
                 <div class="col-md-12 padding-right-zero">
                   <div class="form-group">
-                    <select  name="producto" id="producto" class="form-control" data-rule="required:true" data-msg="Please choose a element">
+                    <select  name="producto" id="producto" class="form-control" data-rule="required:true" data-msg="Por favor seleccione un elemento.">
                       <option value="">Selecciona un producto</option>
                       <option value="Wifi Administrado">Wifi Administrado</option>
                       <option value="Venta  de infraestructura WiFi">Venta  de infraestructura WiFi</option>
-                      <option value="Publicidad con WifiMedia<">Publicidad con WifiMedia</option>
+                      <option value="Publicidad con WifiMedia<">Publicidad con WifiMedia.</option>
                       <option value="Otros">Otros</option>
                     </select>
                     <div class="validation"></div>
@@ -58,10 +58,10 @@
 
                 <div class="col-md-12 padding-right-zero">
                   <div class="form-group">
-                        <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
+                        <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Por favor escriba su mensaje." placeholder="Su mensaje."></textarea>
                         <div class="validation"></div>
                     </div>
-                  <button type="submit" class="btn btn-primary btn-submit">SEND NOW</button>
+                  <button type="submit" class="btn btn-primary btn-submit">Enviar</button>
                 </div>
             </form>
           </div>
